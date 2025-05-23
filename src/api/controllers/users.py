97091953c87716a364954162ce32e89442c65fef
bkliuka <klyuka.bogdan@gmail.com>
2025-05-23 23:56:25 +0300

    Add method for logout

--- src/api/controllers/users.py
+++ src/api/controllers/users.py
@@ -5,6 +5,7 @@ from src.api.request import Request
 
 class Users:
     LOGIN: str = "/users/login"
+    LOGOUT: str = "/users/logout"
     USERS: str = "/users"
 
     def __init__(self, request: Request):
@@ -12,6 +13,9 @@ class Users:
 
     def login(self, login_data: dict) -> APIResponse:
         return self._request.post(path=self.LOGIN, data=login_data)
+
+    def logout(self) -> APIResponse:
+        return self._request.post(path=self.LOGOUT)
 
     def create_user(self, user_data: dict) -> APIResponse:
         return self._request.post(path=self.USERS, data=user_data)
