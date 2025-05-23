4141076ee5e9722170b2a8735a8e6a098a28c2e5
bkliuka <klyuka.bogdan@gmail.com>
2025-05-23 10:39:42 +0300

    Add helper methods

--- src/helpers.py
+++ src/helpers.py
@@ -9,3 +9,7 @@ def get_random_bool() -> bool:
 def get_random_string(length: int = 10) -> str:
     characters = string.ascii_lowercase + string.ascii_uppercase
     return "".join(random.choices(characters, k=length))
+
+
+def get_random_int(start: int = 0, end: int = 999999999) -> int:
+    return random.randint(start, end)
