efee0125de71ba643980c884bdd31de80d15d9b6
bkliuka <klyuka.bogdan@gmail.com>
2025-05-24 11:58:12 +0300

    Add get user profile tests

---
+++ tests/api/users/test_get_user_profile.py
@@ -0,0 +1,27 @@
+from http import HTTPStatus
+
+from assertpy import assert_that
+from jsonschema.validators import validate
+
+from src.api.api_client import APIClient
+from src.api.common import CommonAPIErrors
+from src.responses import user_profile_schema
+from src.settings import config
+
+
+class TestGetUserProfile:
+
+    def test_get_user_profile_without_token_provided(self, unauth_client: APIClient) -> None:
+        response = unauth_client.get_user_profile()
+        response_data = response.json()
+
+        assert response.status == HTTPStatus.UNAUTHORIZED, response_data
+        assert_that(response_data).is_equal_to(dict(error=CommonAPIErrors.NOT_AUTHENTICATE))
+
+    def test_get_user_profile_success(self, auth_client: APIClient) -> None:
+        response = auth_client.get_user_profile()
+        response_data = response.json()
+
+        assert response.status == HTTPStatus.OK, response_data
+        assert_that(response_data).contains_entry({"email": config.user_email})
+        validate(instance=response_data, schema=user_profile_schema)
