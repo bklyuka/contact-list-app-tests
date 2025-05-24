ca60319cb7ab0c3d35a2dcf0ce017761592ada6a
bkliuka <klyuka.bogdan@gmail.com>
2025-05-24 12:08:30 +0300

    Add delete user me test

---
+++ tests/api/users/test_delete_user.py
@@ -0,0 +1,46 @@
+from http import HTTPStatus
+
+import pytest
+from assertpy import assert_that
+
+from src.api.api_client import APIClient
+from src.api.common import CommonAPIErrors
+from src.payloads import CreateUser, Credentials
+
+
+@pytest.fixture(name="credentials")
+def get_creds_of_new_user(unauth_client: APIClient) -> Credentials:
+    credentials_ = Credentials()
+
+    unauth_client.create_user(
+        user_data=CreateUser(
+            email=credentials_.email,
+            password=credentials_.password
+        ).__dict__
+    )
+    return credentials_
+
+
+@pytest.fixture(name="client")
+def get_authenticated_new_client(unauth_client: APIClient, credentials: Credentials) -> APIClient:
+    unauth_client.authenticate(
+        user_email=credentials.email,
+        password=credentials.password
+    )
+    return unauth_client
+
+
+class TestDeleteUser:
+
+    def test_delete_user_me_successfully(self, client: APIClient) -> None:
+        response = client.delete_user_me()
+
+        assert response.status == HTTPStatus.OK
+        assert_that(response.text()).is_empty()
+
+    def test_delete_user_me_without_token_provided(self, unauth_client: APIClient) -> None:
+        response = unauth_client.delete_user_me()
+        response_data = response.json()
+
+        assert response.status == HTTPStatus.UNAUTHORIZED, response_data
+        assert_that(response_data).is_equal_to(dict(error=CommonAPIErrors.NOT_AUTHENTICATE))
