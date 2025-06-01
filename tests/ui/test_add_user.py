2759456228dea72d59191e034460e9a59a4727bc
bkliuka <klyuka.bogdan@gmail.com>
2025-06-01 13:46:28 +0300

    Fixes

--- tests/ui/test_add_user.py
+++ tests/ui/test_add_user.py
@@ -23,7 +23,7 @@ def get_add_user_data() -> CreateUser:
 @pytest.mark.skip_login
 class TestAddUser:
 
-    def test_add_user_with_valid_data(self, add_user_page: AddUserPage, user_data: CreateUser) -> None:
+    def test_add_user_with_valid_data_success(self, add_user_page: AddUserPage, user_data: CreateUser) -> None:
         add_user_page.fill_and_submit_form(
             first_name=user_data.firstName,
             last_name=user_data.lastName,
